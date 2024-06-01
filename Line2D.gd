38a31c3822c8aa61b2cb76449613cdd7b52795a7
kdxiao <kdxiao0203@gmail.com>
2024-06-01 05:21:33 -0400

    a

---
+++ Line2D.gd
@@ -0,0 +1,20 @@
+extends Line2D
+
+var length = 50
+var point = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_position = Vector2(0,0)
+	global_rotation = 0
+	
+	point = get_parent().global_position
+	
+	add_point(point)
+	while get_point_count() > length:
+		remove_point(0)
+	pass
