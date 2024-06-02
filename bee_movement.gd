6b96dece90a04cc9f4fd145fdb803ece4d30141b
Arnav Kumar <arnav.kumar.sri@gmail.com>
2024-06-02 15:29:59 -0400

    color preview

--- bee_movement.gd
+++ bee_movement.gd
@@ -4,6 +4,9 @@ var speed = 0
 var angular_speed = PI
 var in_flower = false
 var curr_flower
+@onready var ci1 = get_node("/root/TileMap/Color1")
+@onready var ci2 = get_node("/root/TileMap/Color2")
+@onready var ci3 = get_node("/root/TileMap/Color3")
 var flowers_visited = 0
 var flower
 var running_color
@@ -35,18 +38,24 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("ui_select"):
 		if flowers_visited == 0 and in_flower:
-			print("here0")
+			print("gere 0")
 			running_color = curr_flower.self_modulate
+			ci1.self_modulate = curr_flower.self_modulate
 			flowers_visited = 1
 		elif flowers_visited == 1 and in_flower:
-			print("here1")
+			print("gere 1")
 			running_color = running_color.lerp(curr_flower.self_modulate, 0.5)
+			ci2.self_modulate = curr_flower.self_modulate
+			ci3.self_modulate = running_color
 			flowers_visited = 2
 		elif flowers_visited == 2 and not in_flower:
-			print("here2")
+			print("here 2")
 			var instance = flower.instantiate()
 			instance.position = global_position
 			instance.self_modulate = running_color
+			ci1.self_modulate = Color8(255, 255, 255)
+			ci2.self_modulate = Color8(255, 255, 255)
+			ci3.self_modulate = Color8(255, 255, 255)
 			get_node("../../").add_child(instance)
 			flowers_visited = 0
 
