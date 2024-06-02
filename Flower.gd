4b6dc17e9946c23ca53e9621857fa260f30c15a8
kdxiao <kdxiao0203@gmail.com>
2024-06-02 03:43:48 -0400

    added flower signals

--- Flower.gd
+++ Flower.gd
@@ -1,17 +1,25 @@
-extends Area2D
+extends Sprite2D
 
-signal _on_bee_on_flower
+signal flower_enter
+signal flower_exit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 	pass
-	
 
-func _on_collision_shape_2d_tree_entered():
-	_on_bee_on_flower.emit()
+
+func _on_flower_body_entered(body):
+	print("flower entered")
+	emit_signal("flower_enter")
+	pass # Replace with function body.
+
+
+func _on_flower_body_exited(body):
+	print("flower exited")
+	emit_signal("flower_exit")
 	pass # Replace with function body.
